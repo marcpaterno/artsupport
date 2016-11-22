aec67e2b1e2ccce46f7f8509f5335b022076b0b9
Marc Paterno <paterno@fnal.gov>
2016-11-22 11:56:36 -0600

    First version for distribution

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,12 @@
 #' Load moduline timing information from a _TimeTracker_ database
 #'
-#' Open the specified _TimeTracker_ database file, and read the
+#' Open the specified \emph{TimeTracker} database file, and read the
 #' module-by-module, event-by-event timing information. The returned dataframe
-#' contains a column "lbl", containing a user-supplied label. The intent is
+#' contains a column \code{lbl}, containing a user-supplied label. The intent is
 #' that this label allows many such dataframes, each with a distinct label,
-#' to be concatenated with `rbind`, and used for comparitive analysis.
+#' to be concatenated with \code{rbind}, and used for comparitive analysis.
 #'
-#' @param filename The name of the _TimeTracker_ database file to open.
+#' @param filename The name of the \emph{TimeTracker} database file to open.
 #' @param lbl The label to be applied to each row in the dataframe.
 #' @return a dataframe
 #' @export
@@ -16,7 +16,10 @@
 #' d2 <- load_module_timing("lsd6/timing.db", "lsd6")
 #' d3 = rbind(d1, d2)
 load_module_timing <- function(filename, lbl) {
-  src_sqlite(filename) %>% tbl("TimeModule") %>% tbl_df %>% cbind(lbl)
+  dplyr::src_sqlite(filename) %>%
+    dplyr::tbl("TimeModule") %>%
+    dplyr::tbl_df %>%
+    cbind(lbl)
 }
 
 #' Make a box-and-whisker plot of module times from a timing dataframe.
@@ -25,10 +28,10 @@ load_module_timing <- function(filename, lbl) {
 #' @return a lattice object, as fron bwplot
 #' @export
 module_bwplot <- function(data, maxLables = NULL, ...) {
-  bwplot(reorder(ModuleLabel, Time)~Time|lbl,
-         data = data,
-         scales = list(x = list( log = 10, equispaced = FALSE)),
-         ...
+  lattice::bwplot(
+    reorder(ModuleLabel, Time) ~ Time | lbl,
+    data = data,
+    scales = list(x = list(log = 10, equispaced = FALSE)),
+    ...
   )
 }
-
