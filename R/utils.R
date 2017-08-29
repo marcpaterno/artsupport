dfe1718353ea2bc3d11f32d3efa998ba2f018258
Marc Paterno <paterno@fnal.gov>
2017-08-28 19:28:15 -0500

    Add reading of MemoryTracker database files

--- R/utils.R
+++ R/utils.R
@@ -22,10 +22,38 @@ load_module_timing <- function(filename, lbl) {
   cbind(lbl)
 }
 
+#' Load memory use information from a \emph{MemoryTracker} database
+#'
+#' Open the specified \emph{MemoryTracker} database file, and read the
+#' specified table name info a dataframe. The returned dataframe contains
+#' a column \code{lbl}, containing a user-supplied label. The intent is that
+#' this label allows many such dataframes, each with a distince label,
+#' to be concatenated with \code{rbind}, and used for comparitive analysis.
+#' Consult the \emph{MemoryTracker} documentation for a list of the tables
+#' that are available.
+#'
+#' @param filename THe name of the \emph{MemoryTracker} data file to open.
+#' @param tablename The name of the table to be read.
+#' @param lbl The label to be applied to each row in the dataframe.
+#' @return a dataframe
+#' @export
+#'
+#' @examples
+#' d1 <- load_memory_use("woof/memory.db", "ModuleInfo", "woof")
+#' d2 <- load_memory_use("woof/memory.db", "EventInfo", "woof")
+#' d3 <- load_memory_use("woof/memory.db", "OtherInfo", "woof")
+#' d4 <- load_memory_use("woof/memory.db", "PeakUsage", "woof")
+load_memory_use <- function(filename, tablename, lbl) {
+  src_sqlite(filename) %>%
+    tbl(tablename) %>%
+    tbl_df %>%
+    cbind(lbl)
+}
+
 #' Make a box-and-whisker plot of module times from a timing dataframe.
 #'
 #' @param data The dataframe to plot
-#' @param maxmodule Plot only the top maxmodules entries from each label
+#' @param maxmodules If non-NULL, plot only the top maxmodules entries from each label
 #'
 #' @return a lattice object, as from bwplot
 #' @export
