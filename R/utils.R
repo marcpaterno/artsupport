f299361973dcb93774d0f76c0196467f8f9d53b7
Marc Paterno <paterno@fnal.gov>
2017-09-21 15:25:52 -0500

    Documentation improvements

--- R/utils.R
+++ R/utils.R
@@ -4,12 +4,12 @@
 
 #' Load a table from an SQLite3 database
 #'
-#' Open the specified \emph{MemoryTracker} database file, and read the specified
-#' table name info a dataframe. If \code{lbl} is non-NULL, the returned
-#' dataframe will contain a column named \code{lbl}, containing the
-#' user-supplied label. The intent is that this label allows many such
-#' dataframes, each with a distinct label, to be concatenated with \code{rbind},
-#' and used for comparitive analysis. Consult the \emph{MemoryTracker}
+#' Open the specified SQLite database file, and read the specified table name
+#' info a dataframe. If \code{lbl} is non-NULL, the returned dataframe will
+#' contain a column named \code{lbl}, containing the user-supplied label. The
+#' intent is that this label allows many such dataframes, each with a distinct
+#' label, to be concatenated with \code{rbind}, and used for comparitive
+#' analysis. Consult the \emph{MemoryTracker} and \emph{TimeTracker}
 #' documentation for a list of the tables that are available.
 #'
 #' @param filename THe name of the SQLite3 database file to open.
@@ -18,7 +18,9 @@
 #' @return a dataframe
 #'
 #' @examples
-#' d <- .load_table("woof/memory.db", "ModuleInfo", "woof")
+#' d1 <- load_table("woof-memory.db", "ModuleInfo", "woof")
+#' d2 <- load_table("mac-memory.db, "ModuleInfo, "mac")
+#' mods <- rbind(d1, d2)
 #'
 .load_table <- function(filename, tablename, lbl) {
   con <- DBI::dbConnect(RSQLite::SQLite(), dbname = filename)
