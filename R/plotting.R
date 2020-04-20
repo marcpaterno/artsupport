803310561741c4aa54bdd44e393da9ae8551bfa0
Marc Paterno <paterno@fnal.gov>
2020-04-20 18:14:48 -0500

    Remove lattice dependency

--- R/plotting.R
+++ R/plotting.R
@@ -7,24 +7,31 @@
 #'
 #' @param data The dataframe to plot, as from \code{load_module_timing}
 #' @param n    If non-NULL, plot only the top \code{n} entries from each label
-#' @param ... additional arguments passed to \code{bwplot}
+#' @param ... additional arguments passed to \code{geom_boxplot}
+#' @param use_log if TRUE, a log scale is used for the $x$ axis
 #'
 #' @return a lattice object, as from bwplot
 #' @export
 #' @importFrom stats median
 #' @importFrom magrittr "%>%"
 #'
-module_bwplot <- function(data, n = NA, ...) {
+module_bwplot <- function(data, n = NULL, use_log = FALSE, ...) {
   checkmate::check_data_frame(data)
-  checkmate::check_count(n, na.ok = TRUE)
+  if (!is.null(n))
+    checkmate::check_count(n)
   # If n is set, subset the data to include only those with the largest
   # median Times.
-  interesting <- if (is.na(n)) data else get_interesting(data, n)
+  interesting <- if (is.null(n))
+    data
+  else
+    get_interesting(data, n)
 
-  lattice::bwplot(
-    stats::reorder(ModuleLabel, Time) ~ Time,
-    data = interesting,
-    scales = list(x = list(log = 10, equispaced = FALSE)),
-    ...
-  )
+  p <- ggplot2::ggplot(interesting,
+                       ggplot2::aes(.data$Time,
+                                    stats::reorder(.data$ModuleLabel, .data$Time))) +
+    ggplot2::geom_boxplot(...) +
+    ggplot2::labs(x = "time (s)", y = "Module label")
+  if (isTRUE(use_log))
+    p <- p + ggplot2::scale_x_log10()
+  p
 }
